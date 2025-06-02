07baf77b6a699ef2a10154aef052a5bda2b66e4b
piragna <gonzalovarelaalagna@gmail.com>
2025-06-02 16:20:48 -0300

    deleting TypeScript content

--- tasks/task2/sol.js
+++ tasks/task2/sol.js
@@ -36,6 +36,15 @@
  * or cater to specific use cases related to primitive types.
  * You are encouraged to explore complex scenarios and push the limits of type conversion.
  */
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addValues = addValues;
 // Function to add two values with type checking
@@ -58,46 +67,141 @@ function addValues(a, b) {
             return a + b;
         }
         else if (typeof b === 'string') {
+            if (isNaN(Number(b)))
+                throw new Error('Cannot add number and string');
             return a + parseFloat(b);
         }
         else if (typeof b === 'boolean') {
-            return a + (b ? 1 : 0);
+            throw new Error('Cannot add number and boolean');
         }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        else if (Array.isArray(b)) {
+            throw new Error('Cannot add number and array');
+        }
+        else if (typeof b === 'object') {
+            if (b === null) {
+                return a;
+            }
+            else if (b instanceof Number) {
+                return a + b.valueOf();
+            }
+            else {
+                throw new Error('Incompatible types for addition');
+            }
+        }
+        else if (typeof b === 'undefined') {
+            return a;
+        }
+        else if (typeof b === 'function') {
+            throw new Error('Cannot add number and function');
+        }
+        else if (b === null) {
+            return a;
+        }
     }
     else if (typeof a === 'string') {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            return a + b;
+        }
+        else if (typeof b === 'string') {
+            return a + b; // Concatenate strings
+        }
+        else if (typeof b === 'boolean') {
+            throw new Error('Cannot add string and boolean');
+        }
+        else if (Array.isArray(b)) {
+            throw new Error('Cannot add string and array');
+        }
+        else if (typeof b === 'object') {
+            if (b === null) {
+                return a;
+            }
+            else if (b instanceof String) {
+                return a + b.valueOf();
+            }
+            else {
+                throw new Error('Incompatible types for addition');
+            }
+        }
+        else if (typeof b === 'undefined') {
+            return a;
+        }
+        else if (typeof b === 'function') {
+            throw new Error('Cannot add string and function');
+        }
+        else if (b === null) {
+            return a;
+        }
     }
     else if (typeof a === 'boolean') {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            throw new Error('Cannot add boolean and number');
+        }
+        else if (typeof b === 'string') {
+            throw new Error('Cannot add boolean and string');
+        }
+        else if (typeof b === 'boolean') {
+            return a && b; // Logical AND for booleans
+        }
+        else if (Array.isArray(b)) {
+            throw new Error('Cannot add boolean and array');
+        }
+        else if (typeof b === 'object') {
+            if (b === null) {
+                return a;
+            }
+            else if (b instanceof Boolean) {
+                return a && b.valueOf(); // Logical AND for booleans
+            }
+            else {
+                throw new Error('Incompatible types for addition');
+            }
+        }
+        else if (typeof b === 'undefined') {
+            return a;
+        }
+        else if (typeof b === 'function') {
+            throw new Error('Cannot add boolean and function');
+        }
+        else if (b === null) {
+            return a;
+        }
     }
     else if (Array.isArray(a)) {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            a.push(b); // Add number to array
+            return a;
+        }
+        else if (typeof b === 'string') {
+            a.push(b); // Add string to array
+            return a;
+        }
+        else if (typeof b === 'boolean') {
+            a.push(b); // Add boolean to array
+            return a;
+        }
+        else if (Array.isArray(b)) {
+            return __spreadArray(__spreadArray([], a, true), b, true); // Concatenate arrays
+        }
+        else if (typeof b === 'object') {
+            if (b === null) {
+                return a;
+            }
+            else if (b instanceof Array) {
+                return __spreadArray(__spreadArray([], a, true), b, true); // Concatenate arrays
+            }
+            else {
+                throw new Error('Incompatible types for addition');
+            }
+        }
+        else if (typeof b === 'undefined') {
+            return a;
+        }
+        else if (typeof b === 'function') {
+            throw new Error('Cannot add array and function');
+        }
+        else if (b === null) {
+            return a;
+        }
     }
     else if (typeof a === 'object') {
         if (typeof b === 'number') { }
@@ -110,34 +214,82 @@ function addValues(a, b) {
         else if (b === null) { }
     }
     else if (typeof a === 'undefined') {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            return b; // If a is undefined, return b
+        }
+        else if (typeof b === 'string') {
+            return b; // If a is undefined, return b
+        }
+        else if (typeof b === 'boolean') {
+            return b; // If a is undefined, return b
+        }
+        else if (Array.isArray(b)) {
+            return b; // If a is undefined, return b
+        }
+        else if (typeof b === 'object') {
+            return b; // If a is undefined, return b
+        }
+        else if (typeof b === 'undefined') {
+            return undefined; // If both are undefined, return undefined
+        }
+        else if (typeof b === 'function') {
+            return b; // If a is undefined, return b
+        }
+        else if (b === null) {
+            return null; // If a is undefined, return b
+        }
     }
     else if (typeof a === 'function') {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            return a.call(b); // Call function a with b as argument
+        }
+        else if (typeof b === 'string') {
+            return a.call(b); // Call function a with b as argument
+        }
+        else if (typeof b === 'boolean') {
+            return a.call(b); // Call function a with b as argument
+        }
+        else if (Array.isArray(b)) {
+            return a.apply(null, b); // Call function a with array b as arguments
+        }
+        else if (typeof b === 'object') {
+            return a.call(b); // Call function a with b as context
+        }
+        else if (typeof b === 'undefined') {
+            return a(); // Call function a with no arguments
+        }
+        else if (typeof b === 'function') {
+            return a.call(b); // Call function a with b as context
+        }
+        else if (b === null) {
+            return a.call(null); // Call function a with null as context
+        }
     }
     else if (a === null) {
-        if (typeof b === 'number') { }
-        else if (typeof b === 'string') { }
-        else if (typeof b === 'boolean') { }
-        else if (Array.isArray(b)) { }
-        else if (typeof b === 'object') { }
-        else if (typeof b === 'undefined') { }
-        else if (typeof b === 'function') { }
-        else if (b === null) { }
+        if (typeof b === 'number') {
+            return b; // If a is null, return b
+        }
+    }
+    else if (typeof b === 'string') {
+        return b; // If a is null, return b
+    }
+    else if (typeof b === 'boolean') {
+        return b; // If a is null, return b
+    }
+    else if (Array.isArray(b)) {
+        return b; // If a is null, return b
+    }
+    else if (typeof b === 'object') {
+        return b; // If a is null, return b
+    }
+    else if (typeof b === 'undefined') {
+        return null; // If a is null, return null
+    }
+    else if (typeof b === 'function') {
+        return b.call(null); // Call function b with null as context
+    }
+    else if (b === null) {
+        return null; // If both are null, return null
     }
 }
 console.log(1 + (true ? 1 : 0)); // 2
