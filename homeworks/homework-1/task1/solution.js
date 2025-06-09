1f1cd44e0f3a6017fcab891a107a0d6edb595ef5
piragna <gonzalovarelaalagna@gmail.com>
2025-06-08 22:47:33 -0300

    reestructuring project structure

--- homeworks/homework-1/task1/solution.js
+++ homeworks/homework-1/task1/solution.js
@@ -1,91 +1,76 @@
 "use strict";
-/**Task: Perform arithmetic operations on strings without relying on bigint or arithmetic libraries.
- * The operations should function as string functions, considering only positive integers
- * (you can avoid negative numbers, all numbers will be positive and integer).
- * `String.plus(string) => string`
- * `String.minus(string) => string`
- * `String.divide(string) => string`
- * `String.multiply(string) => string`
- * */
+/**
+ * Task: Perform arithmetic operations on strings without relying on bigint or arithmetic libraries.
+ * The operations should function as string functions, considering only positive integers.
+ * Implemented as a class with methods: plus, minus, divide, multiply.
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
-String.prototype.plus = function (str) {
-    var ret = "";
-    var a = Number(this.toString());
-    var b = Number(str);
-    if (isNaN(a))
-        ret += ("Invalid number/s: " + this.toString());
-    if (isNaN(b)) {
-        if (ret.length == 0) {
-            ret += "Invalid number/s: " + str;
-        }
-        else {
-            ret += ", " + str;
-        }
-        return ret;
+
+class StringArithmetic {
+    constructor(str) {
+        this.value = str;
     }
-    return (ret.length > 0) ? ret : (a + b).toString();
-};
-String.prototype.minus = function (str) {
-    var ret = "";
-    var a = Number(this.toString());
-    var b = Number(str);
-    if (isNaN(a))
-        ret += ("Invalid number/s: " + this.toString());
-    if (isNaN(b)) {
-        if (ret.length == 0) {
-            ret += "Invalid number/s: " + str;
+
+    plus(str) {
+        const a = Number(this.value);
+        const b = Number(str);
+        if (isNaN(a) || isNaN(b)) {
+            return `Invalid number/s: ${isNaN(a) ? this.value : ''}${isNaN(a) && isNaN(b) ? ', ' : ''}${isNaN(b) ? str : ''}`;
         }
-        else {
-            ret += ", " + str;
-        }
-        return ret;
+        return (a + b).toString();
     }
-    return (ret.length > 0) ? ret : (a - b).toString();
-};
-String.prototype.divide = function (str) {
-    var ret = "";
-    var a = Number(this.toString());
-    var b = Number(str);
-    if (b === 0) {
-        ret += "Division by zero is not allowed.";
-        return ret;
+
+    minus(str) {
+        const a = Number(this.value);
+        const b = Number(str);
+        if (isNaN(a) || isNaN(b)) {
+            return `Invalid number/s: ${isNaN(a) ? this.value : ''}${isNaN(a) && isNaN(b) ? ', ' : ''}${isNaN(b) ? str : ''}`;
+        }
+        return (a - b).toString();
     }
-    if (isNaN(a))
-        ret += ("Invalid number/s: " + this.toString());
-    if (isNaN(b)) {
-        if (ret.length == 0) {
-            ret += "Invalid number/s: " + str;
+
+    divide(str) {
+        const a = Number(this.value);
+        const b = Number(str);
+        if (b === 0) {
+            return "Division by zero is not allowed.";
         }
-        else {
-            ret += ", " + str;
+        if (isNaN(a) || isNaN(b)) {
+            return `Invalid number/s: ${isNaN(a) ? this.value : ''}${isNaN(a) && isNaN(b) ? ', ' : ''}${isNaN(b) ? str : ''}`;
         }
-        return ret;
+        return (a / b).toString();
     }
-    return (ret.length > 0) ? ret : (a / b).toString();
-};
-String.prototype.multiply = function (str) {
-    var ret = "";
-    var a = Number(this.toString());
-    var b = Number(str);
-    if (isNaN(a))
-        ret += "Invalid number/s: " + this.toString();
-    if (isNaN(b)) {
-        if (ret.length == 0) {
-            ret += "Invalid number/s: " + str;
+
+    multiply(str) {
+        const a = Number(this.value);
+        const b = Number(str);
+        if (isNaN(a) || isNaN(b)) {
+            return `Invalid number/s: ${isNaN(a) ? this.value : ''}${isNaN(a) && isNaN(b) ? ', ' : ''}${isNaN(b) ? str : ''}`;
         }
-        else {
-            ret += ", " + str;
-        }
-        return ret;
+        return (a * b).toString();
     }
-    return (ret.length > 0) ? ret : (a * b).toString();
-};
-console.log("1".plus("2")); // "3"
-console.log("10".minus("2")); // "8"
-console.log("10".divide("2")); // "5"
-console.log("10".multiply("2")); // "20"
-console.log("10".plus("abc")); // "Invalid number: 10" and "NaN"
-console.log("1213l1231".multiply("abc")); // "Invalid number: 10" and "NaN"
-console.log("abc".minus("2")); // "Invalid number: NaN" and "NaN"
-console.log("1231231qa".divide("2")); // "Invalid number: NaN" and "NaN"
-console.log("1231231".multiply("2")); // "2462462"
+}
+
+// Example usage:
+const sa1 = new StringArithmetic("1");
+console.log(sa1.plus("2")); // "3"
+
+const sa2 = new StringArithmetic("10");
+console.log(sa2.minus("2")); // "8"
+console.log(sa2.divide("2")); // "5"
+console.log(sa2.multiply("2")); // "20"
+
+const sa3 = new StringArithmetic("10");
+console.log(sa3.plus("abc")); // "Invalid number/s: abc"
+
+const sa4 = new StringArithmetic("1213l1231");
+console.log(sa4.multiply("abc")); // "Invalid number/s: 1213l1231, abc"
+
+const sa5 = new StringArithmetic("abc");
+console.log(sa5.minus("2")); // "Invalid number/s: abc"
+
+const sa6 = new StringArithmetic("1231231qa");
+console.log(sa6.divide("2")); // "Invalid number/s: 1231231qa"
+
+const sa7 = new StringArithmetic("1231231");
+console.log(sa7.multiply("2")); // "2462462"
