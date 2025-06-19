7c3381679395c6a0216484dd1e122aa4765cf8a5
piragna <gonzalovarelaalagna@gmail.com>
2025-06-19 13:58:40 -0300

    working on task1

--- homeworks/homework-4/task1/solution.js
+++ homeworks/homework-4/task1/solution.js
@@ -9,17 +9,15 @@ age: 30
 email: "john.doe@example.com"
 ```
 
-Use property descriptors to make all properties of the person object read-only and non-writable,
-    so their values cannot be changed directly.
-
-Implement a method called `updateInfo` on the person object that takes a new info object as an argument.
-    The info object should contain updated values for any of the properties (e.g., { firstName: "Jane",
-    age: 32 }).
-    Ensure that this method adheres to the read-only property descriptor set earlier.
 
 Create a new property called `address` on the person object with an initial value of an empty object. 
     Make this property non-enumerable and non-configurable. */
 
+
+/**Use property descriptors to make all properties of the person object read-only and non-writable,
+    so their values cannot be changed directly.
+ * 
+ */
 const person = {
     firstName: "John",
     lastName: "Doe",
@@ -27,16 +25,39 @@ const person = {
     email: "john.doe@example.com",
 }
 Object.defineProperties(person, {
-    firstName: {
-        writable:false,
-    },
-    lastName: {
-        writable:false,
-    },
-    age: {
-        writable:false,
-    },
-    email: {
-        writable:false,
-    },
+    firstName: {writable:false,},
+    lastName: {writable:false,},
+    age: {writable:false,},
+    email: {writable:false,},
 });
+/**Implement a method called `updateInfo` on the person object that takes a new info object as an argument.
+    The info object should contain updated values for any of the properties (e.g., { firstName: "Jane",
+    age: 32 }).
+    Ensure that this method adheres to the read-only property descriptor set earlier.
+ */
+Object.defineProperty(person, 'updateInfo', {
+        value:function(info){
+            let updatedPerson = {};
+            let originalDescriptors = Object.getOwnPropertyDescriptors(person);
+            let newDescriptors = {...originalDescriptors};
+            if (info.firstName!==undefined){
+                newDescriptors.firstName.value=info.firstName;
+            }
+            if (info.lastName!==undefined){
+                newDescriptors.lastName.value=info.lastName;
+            }
+            if (info.age!==undefined){
+                newDescriptors.age.value=info.age;
+            }
+            if (info.email!==undefined){
+                newDescriptors.email.value=info.email;
+            }
+            updatedPerson = Object.defineProperties(updatedPerson, newDescriptors);
+            return updatedPerson;
+        },
+        writable:false,
+        enumerable:true,
+        configurable:true
+    }
+);
+console.log(person.updateInfo({firstName:'Alicia'}));
