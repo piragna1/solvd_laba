8e4a3084fa6c83ef7dccd2fbdbba422558933a5b
piragna <gonzalovarelaalagna@gmail.com>
2025-06-19 12:40:48 -0300

    homwork4 task1

--- homeworks/homework-4/task1/solution.js
+++ homeworks/homework-4/task1/solution.js
@@ -9,8 +9,34 @@ age: 30
 email: "john.doe@example.com"
 ```
 
-Use property descriptors to make all properties of the person object read-only and non-writable, so their values cannot be changed directly.
+Use property descriptors to make all properties of the person object read-only and non-writable,
+    so their values cannot be changed directly.
 
-Implement a method called `updateInfo` on the person object that takes a new info object as an argument. The info object should contain updated values for any of the properties (e.g., { firstName: "Jane", age: 32 }). Ensure that this method adheres to the read-only property descriptor set earlier.
+Implement a method called `updateInfo` on the person object that takes a new info object as an argument.
+    The info object should contain updated values for any of the properties (e.g., { firstName: "Jane",
+    age: 32 }).
+    Ensure that this method adheres to the read-only property descriptor set earlier.
 
-Create a new property called `address` on the person object with an initial value of an empty object. Make this property non-enumerable and non-configurable. */
+Create a new property called `address` on the person object with an initial value of an empty object. 
+    Make this property non-enumerable and non-configurable. */
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    email: "john.doe@example.com",
+}
+Object.defineProperties(person, {
+    firstName: {
+        writable:false,
+    },
+    lastName: {
+        writable:false,
+    },
+    age: {
+        writable:false,
+    },
+    email: {
+        writable:false,
+    },
+});
