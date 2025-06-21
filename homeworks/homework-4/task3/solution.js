71fe2929a20b83474c124265bb3118abed5665a1
piragna <gonzalovarelaalagna@gmail.com>
2025-06-21 02:36:50 -0300

    h4 task3
    modularization

--- homeworks/homework-4/task3/solution.js
+++ homeworks/homework-4/task3/solution.js
@@ -2,53 +2,58 @@
  *
  * */
 
-/** Create an object called bankAccount with the following properties and values:
-`balance: 1000 (default value)`
-*/
-const bankAccount = {
-  balance: 1000,
-};
-// console.log(Object.getOwnPropertyDescriptors(bankAccount));
+export class Task3 {
+    constructor() {
+        this.bankAccount = null;
+    }
 
-/** Use a getter to define a property called `formattedBalance`, which returns the balance with a
- *  currency symbol (e.g., "$1000").
- */
-Object.defineProperty(bankAccount, "formattedBalance", {
-  get() {
-    return "$" + this.balance;
-  },
-  enumerable: true,
-  configurable: true,
-});
-// console.log(bankAccount.formattedBalance);
+    /** 1. Create the bankAccount object with a default balance property */
+    createBankAccount(initialBalance = 1000) {
+        this.bankAccount = { balance: initialBalance };
+    }
 
-/** Use a setter to define a property called `balance`, which updates the account balance and automatically updates the corresponding 
-    `formattedBalance` value.
-*/
-Object.defineProperty(bankAccount, "setBalance", {
-  set: function (val) {
-    this.balance = val;
-  },
-});
-// bankAccount.setBalance = 200;
-// console.log(bankAccount.formattedBalance); // 200
+    /** 2. Add a getter for formattedBalance that returns the balance with a currency symbol */
+    addFormattedBalanceGetter() {
+        Object.defineProperty(this.bankAccount, "formattedBalance", {
+            get() {
+                return "$" + this.balance;
+            },
+            enumerable: true,
+            configurable: true,
+        });
+    }
 
-/**Implement a method called `transfer` on the `bankAccount` object that takes two `bankAccount` objects and an amount as arguments. 
-    The method should transfer the specified amount from the current account to the target account. Ensure that the `balance` and 
-    `formattedBalance` properties of both accounts are updated correctly. 
-*/
-Object.defineProperty(bankAccount, "transfer", {
-  value: function (senderAcc, receiverAcc, amount) {
-    senderAcc.setBalance = senderAcc.balance - amount;
-    receiverAcc.setBalance= receiverAcc.balance + amount;
-  },
-  writable: false,
-  enumerable: true,
-  configurable: true,
-});
-// const bankAccount1={balance:200}
-// Object.defineProperty(bankAccount1, 'formattedBalance', Object.getOwnPropertyDescriptor(bankAccount,'formattedBalance'));
-// Object.defineProperty(bankAccount1, 'setBalance', Object.getOwnPropertyDescriptor(bankAccount,'setBalance'));
-// bankAccount.transfer(bankAccount, bankAccount1,bankAccount.balance)
-// console.log('Sender remaining balance:'+bankAccount.formattedBalance)
-// console.log('Receiver remaining balance:'+bankAccount1.formattedBalance)
+    /** 3. Add a setter for balance that updates the balance and formattedBalance */
+    addBalanceSetter() {
+        Object.defineProperty(this.bankAccount, "setBalance", {
+            set: function (val) {
+                this.balance = val;
+            },
+        });
+    }
+
+    /** 4. Implement the transfer method */
+    addTransferMethod() {
+        Object.defineProperty(this.bankAccount, "transfer", {
+            value: function (senderAcc, receiverAcc, amount) {
+                senderAcc.setBalance = senderAcc.balance - amount;
+                receiverAcc.setBalance = receiverAcc.balance + amount;
+            },
+            writable: false,
+            enumerable: true,
+            configurable: true,
+        });
+    }
+
+    getBankAccount() {
+        return this.bankAccount;
+    }
+
+    /** Helper to clone the bankAccount structure for demo */
+    static cloneBankAccount(account) {
+        const clone = { balance: account.balance };
+        Object.defineProperty(clone, 'formattedBalance', Object.getOwnPropertyDescriptor(account, 'formattedBalance'));
+        Object.defineProperty(clone, 'setBalance', Object.getOwnPropertyDescriptor(account, 'setBalance'));
+        return clone;
+    }
+}
