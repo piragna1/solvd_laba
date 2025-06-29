126eb0e03296f2a515a15c2da89e8393b4dacbae
piragna <gonzalovarelaalagna@gmail.com>
2025-06-28 21:08:53 -0300

    h5 task1 implemented on main

---
+++ homeworks/homework-5/main.js
@@ -0,0 +1,30 @@
+import { Task1 } from "./task1/solution.js";
+// // TASK 1---------------------------------------------
+            // //Usage demonstration:
+            // let person = [
+            //     {id:1,name:'Carlos'},
+            //     {id:2,name:'Carlos'},
+            //     {id:3,name:'Alex'},
+            //     {id:4,name:'Alex'},
+            //     {id:5,name:'Willians'},
+            //     {id:6,name:'Willians'},
+            //     {id:2,name:'Willians'},
+            //     {id:3,name:'Ilya'},
+            //     {id:5,name:'Ilya'},
+            //     {id:1,name:'Pawel'},
+            // ];
+            // let prop = 'id';
+            // console.log("\n🟢 1. Creating `customFilterUnique` function...");
+            // let t1 = new Task1();
+            // console.log("\n🟣 2. Executing filtering function on the next array filtering by `id` property:");
+            // console.log(JSON.stringify(person,{},1));
+            // console.log(`\nResult 👇 \n`,t1.usingCustomFilterUnique(person,(item)=>item[prop]));
+//------
+// TASK 2---------------------------------------------
+//------
+// TASK 3---------------------------------------------
+//------
+// TASK 4---------------------------------------------
+//------
+// TASK 5---------------------------------------------
+//------
