5965ed14d920c8e3cdd355133cc6fe8f9146f0d4
piragna <gonzalovarelaalagna@gmail.com>
2025-06-30 20:01:53 -0300

    starting with homework5 task5

---
+++ homeworks/homework-5/task5/solution.mjs
@@ -0,0 +1,12 @@
+/**### **Task 5: Array Performance Analysis***/
+export class Task5{
+    /**1. Implement a function called `measureArrayPerformance` that takes a function and an array as arguments.
+     *  The `measureArrayPerformance` function should execute the provided function with the given array as input
+     *  and measure the execution time.*/
+    measureArrayPerformance(){
+
+    }
+    /**2. Use the `measureArrayPerformance` function to compare the performance of built-in array methods
+     *  (`map`, `filter`, `reduce`, etc.) against your custom array manipulation functions. */
+}
+    
