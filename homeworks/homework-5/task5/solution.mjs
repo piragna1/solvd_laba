ad2eb236eb8320ff387641f74375a9f14ee93bc1
piragna <gonzalovarelaalagna@gmail.com>
2025-07-01 00:02:15 -0300

    Starting with h5 task5 resolution

--- homeworks/homework-5/task5/solution.mjs
+++ homeworks/homework-5/task5/solution.mjs
@@ -3,10 +3,16 @@ export class Task5{
     /**1. Implement a function called `measureArrayPerformance` that takes a function and an array as arguments.
      *  The `measureArrayPerformance` function should execute the provided function with the given array as input
      *  and measure the execution time.*/
-    measureArrayPerformance(){
-
+    measureArrayPerformance(fn, array){
+        let beggining;
+        let end;
+        beggining=performance.now()
+        fn(array);
+        end=performance.now()
+        console.log(`The process took ${end-beggining} miliseconds to get executed.`);
     }
     /**2. Use the `measureArrayPerformance` function to compare the performance of built-in array methods
      *  (`map`, `filter`, `reduce`, etc.) against your custom array manipulation functions. */
+    
 }
     
