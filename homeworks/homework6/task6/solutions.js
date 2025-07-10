3f13d719427246b2d40ba221a44c4a0aff1d837d
piragna <gonzalovarelaalagna@gmail.com>
2025-07-10 20:20:13 -0300

    Homework6 task6

--- homeworks/homework6/task6/solutions.js
+++ homeworks/homework6/task6/solutions.js
@@ -13,3 +13,28 @@ Your task is to implement a currying function that converts a given function int
 Once all arguments are received, the original function should be executed with the collected arguments.
 3. If the curried function is invoked with fewer arguments than `arity`, it should return a new curried function that waits for the 
 remaining arguments. */
+
+function multiply(a, b, c) {
+	return a * b * c;
+}
+
+function curry(func,arity){
+    return function curried(...args){
+        if (args.length < arity){
+            return function(...nextArgs){
+                return curried(...args,...nextArgs);
+            };
+        }
+        else {
+            return func(...args)
+        }
+    }
+}
+
+const curriedMultiply = curry(multiply, 3);
+
+const step1 = curriedMultiply(2); // Returns a curried function
+const step2 = step1(3); // Returns a curried function
+const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24
+
+console.log("Result:", result); // Expected: 24
