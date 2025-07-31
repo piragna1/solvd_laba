af5ea8bea291e77d45b6a4ded77f918799eb148b
piragna <gonzalovarelaalagna@gmail.com>
2025-07-31 16:51:54 -0300

    Stack class' implementation

--- homeworks/homework-9/part1/part1.js
+++ homeworks/homework-9/part1/part1.js
@@ -1,28 +1,29 @@
 class Stack {
-
   #stack; //private field!
 
-  constructor(collection) { //constructor
+  constructor(collection) {
+    //constructor
     this.#stack = [].concat(Array.from(collection));
   }
 
   //method for adding new elements
-  push(element){
+  push(element) {
     this.#stack.push(element);
   }
 
   //method for removing and retrieving the element at the top of the stack
-  pop(){
-    let ret = this.#stack[this.#stack.length-1];//last element
-    this.#stack.length = this.#stack.length-1;//removing last element
-    return ret;
+  pop() {
+    if (this.#stack.length === 0) return undefined;
+    let ret = this.#stack[this.#stack.length - 1]; //last element
+    this.#stack.length = this.#stack.length - 1; //removing last element
+    return ret; //returning removed last element
   }
 
   //method for retrieving the current element at the top without removing it.
-  peek(){
-    return this.#stack[this.#stack.length-1];
+  peek() {
+    if (this.#stack.length === 0) return undefined;
+    return this.#stack[this.#stack.length - 1]; //returning current last element
   }
-
 }
 class Queue {}
 class BinaryTree {}
@@ -50,28 +51,28 @@ class LinkedList {}
 
 // // console.log([][Symbol.iterator]); //[Function: values]
 // // console.log({}[Symbol.iterator]); //undefined
-//---------------------------------- 
-
+//----------------------------------
 
 //--------------------------------Using Stack class' implementation
-const fruits = new Map([
-  ["apples", 500],
-  ["bananas", 300],
-  ["oranges", 200],
-]);
+// const fruits = new Map([
+//   ["apples", 500],
+//   ["bananas", 300],
+//   ["oranges", 200],
+// ]);
 
-const ownStack = new Stack(fruits);
-console.log(ownStack._stack)//not accessible
-console.log(ownStack);//not accessible
-console.log(ownStack.peek());//[ 'oranges', 200 ]
-ownStack._stack = [];//does not work
-console.log(ownStack.peek());//[ 'oranges', 200 ]
-console.log(ownStack.pop())//[ 'oranges', 200 ]
-console.log(ownStack.peek());//[ 'bananas', 300 ]
-console.log(ownStack.pop())//[ 'bananas', 300 ]
-console.log(ownStack.pop())//[ 'apples', 500 ]
-// console.log(ownStack.pop())//RangeError: Invalid array length
-//reimplementing pop method
+// const ownStack = new Stack(fruits);
+// console.log(ownStack._stack)//not accessible
+// console.log(ownStack);//not accessible
+// console.log(ownStack.peek());//[ 'oranges', 200 ]
+// ownStack._stack = [];//does not work
+// console.log(ownStack.peek());//[ 'oranges', 200 ]
+// console.log(ownStack.pop())//[ 'oranges', 200 ]
+// console.log(ownStack.peek());//[ 'bananas', 300 ]
+// console.log(ownStack.pop())//[ 'bananas', 300 ]
+// console.log(ownStack.pop())//[ 'apples', 500 ]
+// // console.log(ownStack.pop())//before: RangeError: Invalid array length
+// //reimplementing pop method
+// console.log(ownStack.pop())// after: undefined
 //------------------------------------------------------
 
 //---------------------------------------------
